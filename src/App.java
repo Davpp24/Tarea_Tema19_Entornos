04588968d0e70ae041a0d37e5a544182fe91b113
Davpp24 <pelaezperezdavid@gmail.com>
2024-05-04 13:08:37 +0200

    C1

--- src/App.java
+++ src/App.java
@@ -1,6 +1,4 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, main");
-        System.out.println("Este Sout es para simular que un compañero ha hecho un commit y lo ha sibido y nosotros estabamos trabajando sobre el commit anterior");
-    }
+        System.out.println("Hello, Feature");
 }
