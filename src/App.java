0323a2210a6aa136c45fbffedcbcaaffa168a167
Davpp24 <153532577+Davpp24@users.noreply.github.com>
2024-05-04 11:03:18 +0200

    C3

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hola o/main!");
-        System.out.println("Este Sout esta hecho desde el repositorio remoto");
+        System.out.println("Este Sout esta hecho para comprobar que el pull hace lo mismo que el fetch");
     }
 }
