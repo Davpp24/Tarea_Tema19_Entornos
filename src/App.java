3a05b33285b2c928cbec0fab746fde4346a6d962
Davpp24 <153532577+Davpp24@users.noreply.github.com>
2024-05-04 11:40:53 +0200

    C4

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hola o/main!");
-        System.out.println("Este es el arpartado 5");
+        System.out.println("Este es el arpartado 5 me ha resultado dificil pero lo he sacado");
     }
 }
