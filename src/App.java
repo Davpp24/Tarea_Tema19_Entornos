1b9b647ed22dd05267d3f2468a27e050cac405e7
Davpp24 <153532577+Davpp24@users.noreply.github.com>
2024-05-04 12:16:08 +0200

    C3

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, main");
-        System.out.println("Este Sout es para hacer un cambio para poder realizar un Push");
+        System.out.println("Este Sout es para simular que un compañero ha hecho un commit y lo ha sibido y nosotros estabamos trabajando sobre el commit anterior");
     }
 }
